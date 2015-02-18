98c8b2a4587973628efff303382545acf3980d50
Adrien Ogor <adrien.ogor@zenika.com>
2015-02-18 23:25:52 +0100

    Execution en tant que script global mdconvert

--- convert.js
+++ convert.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var sax = require("sax");
@@ -6,23 +10,28 @@ var _ = require('lodash');
 
 var argv = require('minimist')(process.argv.slice(2));
 
-if (!argv['d'] && !argv['f']) {
-    console.log("no file or dir, use -d or -f argument");
+console.log(process.cwd());
+
+if (argv['h'] || argv['help']) {
+    console.log("usage :\n\
+        mdconvert [-d SOURCE_DIRECTORY |-f FILE_SOURCE] [-o OUTPUT_DIR] \n\n\
+        By default source directory is current dir and target directory is target");
     process.exit(1);
 }
 
 var outDir = argv['o'] || 'target';
 var imgDir = 'ressources/images';
 
-var inDir = argv["d"];
+var inDir = argv["d"] || process.cwd();
 var inFile = argv["f"];
 
-if (inDir && (!fs.existsSync(inDir) || !fs.statSync(inDir).isDirectory())) {
-    console.log("directory not found", inDir);
+if (inFile && !fs.existsSync(inFile)) {
+    console.log("file not found", inFile);
     process.exit(1);
 }
-if (!inDir && !fs.existsSync(inFile)) {
-    console.log("file not found", inFile);
+
+if (!inFile && (!fs.existsSync(inDir) || !fs.statSync(inDir).isDirectory())) {
+    console.log("directory not found", inDir);
     process.exit(1);
 }
 
@@ -35,82 +44,80 @@ function MDWriter() {
     this.mainTitleAdded = false;
     this.firstPage = false;
 
-    this.addPage = function () {
+    this.addPage = function() {
         this.text += "\n\n\n\n";
         this.titreAdded = false;
         this.firstPage = true;
     };
 
-    this.addParagraph = function () {
+    this.addParagraph = function() {
         if (this.inCodeBlock || this.inNotes) {
             this.text += "\n";
         }
     };
 
-    this.addText = function (text) {
-        if(this.firstPage && !this.mainTitleAdded) {
+    this.addText = function(text) {
+        if (this.firstPage && !this.mainTitleAdded) {
             this.text = '#' + text + '\n\n<!-- .slide: class="page-title" -->\n\n\n\n';
             this.mainTitleAdded = true;
-        }
-        else if (!this.titreAdded) {
+        } else if (!this.titreAdded) {
             this.text += "## " + text + "\n";
             this.titreAdded = true;
-        }
-        else {
+        } else {
             this.text += text;
         }
     };
 
-    this.addImage = function (path) {
+    this.addImage = function(path) {
         this.text += "\n\n![](ressources/images/" + path.replace("Pictures/", "") + ")\n";
     };
 
-    this.startList = function () {
+    this.startList = function() {
         this.listLevel++;
-        if (this.listLevel == 1 && this.text[this.text.length-1] !== '\n') {
+        if (this.listLevel == 1 && this.text[this.text.length - 1] !== '\n') {
             this.text += '\n';
         }
     };
 
-    this.endList = function () {
+    this.endList = function() {
         this.listLevel--;
     };
 
-    this.addBullet = function () {
+    this.addBullet = function() {
         if (!this.listLevel) {
             console.warning("not in a list");
         }
         this.text += "\n" + new Array(this.listLevel).join("\t") + "- ";
     };
 
-    this.addEnd = function () {
+    this.addEnd = function() {
         this.text += "\n\n\n\n<!-- .slide: class=\"page-questions\" -->\n\n\n\n<!-- .slide: class=\"page-tp1\" -->";
     };
 
-    this.startCodeBlock = function () {
+    this.startCodeBlock = function() {
         this.tmp = this.text;
         this.text = "";
-        if (this.tmp[this.tmp.length-1] !== '\n') {
+        if (this.tmp[this.tmp.length - 1] !== '\n') {
             this.text += "\n";
         }
         this.text += "\n```";
         this.inCodeBlock = true;
     };
 
-    this.endCodeBlock = function () {
+    this.endCodeBlock = function() {
         this.text += "\n```\n";
         this.inCodeBlock = false;
         var codeBlock = this.text;
         this.text = this.tmp;
-        this.text += codeBlock.replace(/\n{3}/g,"\n\n");
+        this.text += codeBlock.replace(/\n{3}/g, "\n\n");
     };
 
-    this.startNotes = function () {
+    this.startNotes = function() {
         this.text += "\nNotes :\n";
         this.inNotes = true;
     };
 
-    this.endNotes = function () {
+    this.endNotes = function() {
         this.inNotes = false;
     };
 
@@ -146,14 +153,14 @@ function MDWriter() {
 function OdpConverter(filepath, outDir) {
 
     this.filePath = filepath;
-    this.name = path.basename(this.filePath, '.odp').replace(/\s+/g, '_');
+    this.name = path.basename(this.filePath, '.odp').toLowerCase().replace(/[\s-]+/g, '_').replace(/_{2,}/g, "_");
     this.contentName = path.join(outDir, this.name + '-content.xml');
 
-    this.extract = function () {
+    this.extract = function() {
         console.log('extract : ' + this.filePath);
         var zip = new AdmZip(this.filePath);
         var zipEntries = zip.getEntries();
-        zipEntries.forEach(function (val) {
+        zipEntries.forEach(function(val) {
             if (val.entryName.indexOf("content.xml") >= 0) {
                 //console.log(val.entryName);
                 zip.extractEntryTo(val, outDir, true, true);
@@ -168,7 +175,7 @@ function OdpConverter(filepath, outDir) {
         }.bind(this));
     };
 
-    this.convert = function () {
+    this.convert = function() {
         console.log('convert : ' + this.name);
         var mdWriter = new MDWriter();
         var outName = this.name;
@@ -179,7 +186,7 @@ function OdpConverter(filepath, outDir) {
             lowercase: true
         });
 
-        saxStream.on("opentag", function (node) {
+        saxStream.on("opentag", function(node) {
 
             if (node.name === "draw:page") {
                 mdWriter.addPage();
@@ -202,7 +209,7 @@ function OdpConverter(filepath, outDir) {
             }
         });
 
-        saxStream.on("closetag", function (node) {
+        saxStream.on("closetag", function(node) {
 
             if (node === "draw:custom-shape" || node === "draw:rect") {
                 mdWriter.endCodeBlock();
@@ -217,37 +224,50 @@ function OdpConverter(filepath, outDir) {
             }
         });
 
-        saxStream.on("end", function () {
+        saxStream.on("end", function() {
             mdWriter.addEnd();
             console.log('write to ' + path.join(outDir, outName + '.md'));
             fs.writeFile(path.join(outDir, outName + '.md'), mdWriter.text);
+            
         });
 
-        saxStream.on("text", function (t) {
+        saxStream.on("text", function(t) {
             mdWriter.addText(t);
         });
 
-        fs.createReadStream(this.contentName).pipe(saxStream);
+        var readStream = fs.createReadStream(this.contentName).pipe(saxStream);
+
+        readStream.on("end", function() {
+            fs.unlink(this.contentName);
+        }.bind(this));
     }
 }
 
-if(inDir) {
+if (inDir) {
     var outSlidesJson = [];
-    _.forEach(
-        _.filter(
-            fs.readdirSync(inDir),
-            function (file) {
+    fs.readdir(inDir, function(err, files) {
+        var filteredFiles = _.filter(
+            files,
+            function(file) {
                 return path.extname(file) === '.odp';
             }
-        ),
-        function (odp) {
-            var conv = new OdpConverter(path.join(inDir, odp), outDir);
-            outSlidesJson.push(conv.name + '.md');
-            conv.extract();
-            conv.convert();
+        )
+
+        if (!filteredFiles.length) {
+            console.log("No odp files found in : ", inDir);
+            process.exit(1);
         }
-    );
-    fs.writeFile(path.join(outDir, 'slides.json'), JSON.stringify(outSlidesJson, null, 2));
+
+        _.forEach(filteredFiles,
+            function(odp) {
+                var conv = new OdpConverter(path.join(inDir, odp), outDir);
+                outSlidesJson.push(conv.name + '.md');
+                conv.extract();
+                conv.convert();
+            }
+        );
+        fs.writeFile(path.join(outDir, 'slides.json'), JSON.stringify(outSlidesJson, null, 2));
+    });
 } else {
     var conv = new OdpConverter(inFile, outDir);
     conv.extract();
